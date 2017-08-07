d247761fb4793afbc1ee1a65fffd693a7841c2c4
rcdc1989 <raphaelcapon@gmail.com>
2017-08-07 02:50:43 +0000

    improve single-entry view: added back/forth navigation between entries with non-contiguous ID numbers

--- blog/views.py
+++ blog/views.py
@@ -41,15 +41,33 @@ def view_entry(ID=1):
     #In the database, ID is indexed at 1. 
     #This is OK, here we assume that the user knows the ID number from database
     entry = session.query(Entry).get(ID)
-    count = session.query(Entry).count()
-    has_prev = ID > 1
-    has_next = ID < count
+    query_list = session.query(Entry.id).order_by(Entry.id).all()
+    entries_id = [i[0] for i in query_list]
     
+    #we increment the index number to get position that is aligned with "ID"
+    position = entries_id.index(ID) 
+    count = session.query(Entry).count()
+    
+    #to handle case where id numbers are not contiguous (because of deletions)
+    has_prev = position > 0 #check entry is not first in the list
+    has_next = position < count - 2 #check entry is not last in the list
+    
+    try:
+        next_id = entries_id[position + 1]#get id for next entry
+        prev_id = entries_id[position - 1]
+    
+    except IndexError:
+        
+        next_id = ID
+        prev_id = ID
+        
     return render_template("single_entry.html",
         entry=entry,
         ID=ID,
         has_next=has_next,
-        has_prev=has_prev
+        has_prev=has_prev,
+        next_id = next_id,
+        prev_id = prev_id
     )
 
 #view for "add entry" form
